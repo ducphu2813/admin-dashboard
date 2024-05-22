bca102e7745cae2b4cc250cf403d5e08a8d5d134
ducphu2813 <taducphu2813.se@gmail.com>
2024-05-22 10:42:44 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+
+import './index.css'
+import Home from "./pages/home/Home.jsx";
+import {Users} from "./pages/users/Users.jsx";
+import {Products} from "./pages/products/Products.jsx";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet,
+} from "react-router-dom";
+import {Navbar} from "./components/navbar/Navbar.jsx";
+import {Footer} from "./components/footer/Footer.jsx";
+import {Menu} from "./components/menu/Menu.jsx";
+import {Login} from "./pages/login/Login.jsx";
+import "./styles/global.scss";
+function App() {
+
+    //tạo layout
+    const Layout = () => {
+        return (
+          <div className="main">
+              <Navbar />
+              <div className="container">
+
+                  <div className="menuContainer">
+                      <Menu />
+                  </div>
+
+                  <div className="contentContainer">
+                    <Outlet />
+                  </div>
+              </div>
+              <Footer/>
+          </div>
+        );
+    }
+
+    //tạo router
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />
+                },
+                {
+                    path: "users",
+                    element: <Users />
+                },
+                {
+                    path: "products",
+                    element: <Products />
+                }
+            ]
+        },
+        {
+            path: "/login",
+            element: <Login />
+        }
+    ]);
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./navbar.scss"
+export const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <img src="admin.png" alt=""/>
+                <span>React Admin</span>
+            </div>
+
+            <div className="icon">
+
+            </div>
+        </div>
+    )
+}
